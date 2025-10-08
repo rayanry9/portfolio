c0d371edb7bb409807c30f7f552ca2c66e31f628
rayanry9 <rayanhldr@gmail.com>
2025-10-08 23:39:54 +0530

    feat: update

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,8 @@
-<h1>Rayan's Site</h1>
-<h3>In Progress</h3>
+<div class="flex h-screen w-screen flex-col items-center justify-center">
+	<h1 class="text-3xl font-bold">Rayan's Site</h1>
+	<h3>In Progress</h3>
+
+	<p class="mt-12 px-32">
+		This person is currently suffering through exams and is unable to work on this site.
+	</p>
+</div>
