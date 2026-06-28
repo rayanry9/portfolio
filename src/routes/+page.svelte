8b6d9b7593e61372c9d51b2b88e38661edc9e72f
Rayan Halder <rayanhldr@gmail.com>
2026-06-28 23:13:33 +0530

    socials"

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,4 +5,9 @@
 	<p class="mt-12 px-32">
 		This person is currently suffering through exams and is unable to work on this site.
 	</p>
+
+	<div class="mt-12 flex flex-row gap-x-8">
+		<a href="https://github.com/rayanry9">Github</a>
+		<a href="https://linkedin.com/in/rayanh9">Github</a>
+	</div>
 </div>
